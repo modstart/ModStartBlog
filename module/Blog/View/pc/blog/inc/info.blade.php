c37781d1ddd00150ebb6b6ad53c07973570aabc3
ModStart <modstart@163.com>
2022-05-28 10:18:52 +0800

    init

--- module/Blog/View/pc/blog/inc/info.blade.php
+++ module/Blog/View/pc/blog/inc/info.blade.php
@@ -1,9 +1,9 @@
-<div class="tw-bg-white tw-rounded tw-p-6 margin-top">
+<div class="tw-bg-white tw-rounded tw-px-6 tw-py-12 margin-top">
     <div>
         <div class="ub-cover-1-1 contain tw-rounded tw-w-28 tw-m-auto tw-inline-block"
              style="background-image:url({{\ModStart\Core\Assets\AssetsUtil::fix(modstart_config('Blog_Avatar'))}})"></div>
     </div>
-    <div class="tw-text-center tw-text-xl tw-mt-4">
+    <div class="tw-text-center tw-text-xl tw-mt-5">
         {{modstart_config('Blog_Name')}}
     </div>
     <div class="tw-text-center tw-text-gray-400 tw-mt-4">
