74b38af2cfab262346d5ed2c6fbf25ba3da7a0b8
MZ <modstart@163.com>
2025-11-27 11:11:48 +0800

    feat: partner member support

--- module/Partner/Web/routes.php
+++ module/Partner/Web/routes.php
@@ -1,5 +1,12 @@
 <?php
 /* @var \Illuminate\Routing\Router $router */
-
-$router->match(['get'], 'partner', 'PartnerController@index');
+$middleware = [];
+if (class_exists(\Module\Member\Middleware\WebAuthMiddleware::class)) {
+    $middleware[] = \Module\Member\Middleware\WebAuthMiddleware::class;
+}
+$router->group([
+    'middleware' => $middleware,
+], function () use ($router) {
+    $router->match(['get'], 'partner', 'PartnerController@index');
+});
 
